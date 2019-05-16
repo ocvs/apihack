16b45012b2b26cd038a7a564e00f4be004ade283
oc <ocvs00@gmail.com>
2019-05-16 09:54:21 -0300

    mais testes e refatora view

--- apihack/core/tests/test_view_home.py
+++ apihack/core/tests/test_view_home.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from apihack.core.models import Book, Author
 from apihack.core.views import home
 from django.core.serializers import serialize
+from pprint import pprint
 
 
 class ViewHomeTest(TestCase):
@@ -19,8 +20,17 @@ class ViewHomeTest(TestCase):
             "fields": {"author": self.author.pk, "title": self.book.title, "pages": self.book.pages}
         }]
 
+        self.xml_author = f'<?xml version="1.0" encoding="utf-8"?>\n<django-objects version="1.0"><object model="core.author" pk="{self.author.pk}"><field name="name" type="CharField">{self.author.name}</field><field name="age" type="IntegerField">{self.author.age}</field></object></django-objects>'
+
+
+        self.xml_book = f'<?xml version="1.0" encoding="utf-8"?>\n<django-objects version="1.0"><object model="core.book" pk="{self.book.pk}"><field name="author" rel="ManyToOneRel" to="core.author">{self.book.author.pk}</field><field name="title" type="CharField">{self.book.title}</field><field name="pages" type="IntegerField">{self.book.pages}</field></object></django-objects>'
+
         self.response_author = self.client.get(f"/core/author/{self.author.pk}/")
         self.response_book = self.client.get(f"/core/book/{self.book.pk}/")
+        self.response_author_xml = self.client.get(f"/core/author/{self.author.pk}/?fmt=xml")
+        self.response_book_xml = self.client.get(f"/core/book/{self.book.pk}/?fmt=xml")
+
+
 
     def test_get_home_author(self):
         self.assertEqual(200, self.response_author.status_code)
@@ -28,17 +38,35 @@ class ViewHomeTest(TestCase):
     def test_get_home_book(self):
         self.assertEqual(200, self.response_book.status_code)
 
-    def test_serialized_author(self):
-        '''test serialize data author'''
+    def test_json_serialized_author(self):
+        '''test serialize json data author'''
         author_serialized = serialize('json', Author.objects.all())
         self.assertJSONEqual(author_serialized, self.json_author)
 
-    def test_serialized_book(self):
-        '''test serialize data book'''
+    def test_json_serialized_book(self):
+        '''test serialize json data book'''
         book_serialized = serialize('json', Book.objects.all())
         self.assertJSONEqual(book_serialized, self.json_book)
 
     def test_json_author(self):
         '''GET /core/author/1/ de retornar json'''
-        print(self.response_author)
-        #self.assertJSONEqual(self.response_author.GET(), self.json_author)
+        self.assertJSONEqual(self.response_author.content, self.json_author)
+
+    def test_json_book(self):
+        '''GET /core/book/1/ de retornar json'''
+        self.assertJSONEqual(self.response_book.content, self.json_book)
+
+    def test_xml_serialized_author(self):
+        '''test serialize xml_ data author'''
+        author_serialized = serialize('xml', Author.objects.all())
+
+        self.assertXMLEqual(author_serialized, self.xml_author)
+
+    def test_xml_serialized_book(self):
+        '''test serialize xml_ data book'''
+        book_serialized = serialize('xml', Book.objects.all())
+        self.assertXMLEqual(book_serialized, self.xml_book)
+
+
+
+
--- apihack/core/views.py
+++ apihack/core/views.py
@@ -1,14 +1,21 @@
 from django.apps import apps
 from django.http import HttpResponse
 from django.core.serializers import serialize
+from collections import namedtuple
+import asyncio
 
 
 def home(request, app, model, pk=None):
-    rel = request.GET.get('rel', None)
-    fmt = request.GET.get('fmt', 'json')
+    opcoes = verifica_opcoes(request, app, model, pk)
+    return HttpResponse(serialize(opcoes.fmt, opcoes.qs), content_type=f'application/{opcoes.fmt}')
 
+
+def verifica_opcoes(request, app, model, pk):
     model = apps.get_model(f'{app}.{model}')
     qs = model.objects.all()
+
+    rel = request.GET.get('rel', None)
+    fmt = request.GET.get('fmt', 'json')
 
     if pk:
         qs = qs.filter(pk=pk)
@@ -17,5 +24,6 @@ def home(request, app, model, pk=None):
         related = rel.split(',')
         qs = qs.select_related(*related)
 
-    return HttpResponse(serialize(fmt, qs), content_type=f'application/{fmt}')
+    opcoes = namedtuple('Opcoes', ['fmt', 'qs'])
 
+    return opcoes(fmt, qs)
